1c1f5e52bb98ecf5fcbe60692c62bd55150ea0af
AtesiT <atesit@mail.ru>
2025-08-06 18:22:24 +0300

    add rounded corners of views (yellow has a bug) and working button

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -2,12 +2,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var changeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redLight.layer.cornerRadius = 100
+        yellowLight.layer.cornerRadius = 100
+        greenLight.layer.cornerRadius = 100
     }
         
-
+    @IBAction func changeColorButton(_ sender: UIButton) {
+        
+        if redLight.isHidden {
+            yellowLight.isHidden = true
+            redLight.isHidden = false
+        } else if yellowLight.isHidden {
+            greenLight.isHidden = true
+            yellowLight.isHidden = false
+        } else {
+            redLight.isHidden = true
+            greenLight.isHidden = false
+        }
+        
+    }
+    
 }
 
