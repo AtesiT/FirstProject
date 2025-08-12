0706ba72e27a821ac5911de88076d8da3f46ff1e
AtesiT <atesit@mail.ru>
2025-08-12 11:26:07 +0300

    fix rounds on different sizes of screen phones, replace if-else on switch, add enum

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -1,34 +1,58 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     @IBOutlet var changeColorButton: UIButton!
     
+    private var currentLight = CurrentLight.red
+    let lightIsOff = 0.2
+    let lightIsOn = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLight.layer.cornerRadius = 100
-        yellowLight.layer.cornerRadius = 100
-        greenLight.layer.cornerRadius = 100
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        print("Размер стороны квадрата: \(redLight.frame.width)")
     }
         
-    @IBAction func changeColorButton(_ sender: UIButton) {
-        
-        if redLight.isHidden {
-            yellowLight.isHidden = true
-            redLight.isHidden = false
-        } else if yellowLight.isHidden {
-            greenLight.isHidden = true
-            yellowLight.isHidden = false
-        } else {
-            redLight.isHidden = true
-            greenLight.isHidden = false
+    @IBAction func changeColorButtonAction() {
+        if changeColorButton.currentTitle == "START" {
+            changeColorButton.setTitle("Change Color", for: .normal)
         }
         
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
     }
     
 }
 
+// MARK: Enum of CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
